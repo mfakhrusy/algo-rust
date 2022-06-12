e2e8e4f110b1de933a234997b611ee8d7d1624f4
M Fahru <fakhrusy.m@gmail.com>
2022-06-12 15:13:22 -0400

    remove unneeded things

--- count_inversion/src/main.rs
+++ count_inversion/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 fn main() {
-    let mut data = read_data("count_inversion/test_files/big.txt");
+    let data = read_data("count_inversion/test_files/big.txt");
     count_inversion(data);
 }
 
@@ -42,7 +42,7 @@ fn sort(arr: Vec<i32>, mut total_count: &mut u32) -> Vec<i32> {
     return merged_array;
 }
 
-fn merge(left_arr: Vec<i32>, right_arr: Vec<i32>, mut total_count: &mut u32) -> Vec<i32> {
+fn merge(left_arr: Vec<i32>, right_arr: Vec<i32>, total_count: &mut u32) -> Vec<i32> {
     let total_length = left_arr.len() + right_arr.len();
     let mut merged_array = vec![];
     let mut main_index = 0;
@@ -53,7 +53,7 @@ fn merge(left_arr: Vec<i32>, right_arr: Vec<i32>, mut total_count: &mut u32) -> 
     while main_index < total_length {
         if right_arr[right_index] < left_arr[left_index] {
             merged_array.push(right_arr[right_index]);
-            *total_count += (left_arr.len() as u32 - left_index as u32);
+            *total_count += left_arr.len() as u32 - left_index as u32;
             right_index += 1;
         } else {
             merged_array.push(left_arr[left_index]);
@@ -63,7 +63,6 @@ fn merge(left_arr: Vec<i32>, right_arr: Vec<i32>, mut total_count: &mut u32) -> 
         if right_index >= right_arr.len() {
             let mut rest_left_arr = left_arr[left_index..].to_vec();
             main_index += rest_left_arr.len();
-            // *total_count += 1;
             merged_array.append(&mut rest_left_arr);
         } else if left_index >= left_arr.len() {
             let mut rest_right_arr: Vec<i32> = right_arr[right_index..].to_vec();
